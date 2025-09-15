c85250ef8eb7be0859d97eb4f4bcd729ca266c7b
Armstrong-Capital-Financial-Services <divyanshu@armstrong-cap.com>
2025-09-15 19:28:29 +0530

    Update Direct_Equity_Analysis_2.py

--- Direct_Equity_Analysis_2.py
+++ Direct_Equity_Analysis_2.py
@@ -722,7 +722,9 @@ if uploaded_file is not None:
 
     portfolio_df = portfolio_df.fillna(0)
     portfolio_df['Stock Code'] = portfolio_df['Stock Code'].astype(str) + '.NS'
-    portfolio_df.loc[10, 'Stock Code'] = portfolio_df.loc[10, 'Stock Code'].replace('.NS', '.BO')  # Fix for the second stock code
+    portfolio_df.loc[portfolio_df.index[-1], 'Stock Code'] = (
+    portfolio_df.loc[portfolio_df.index[-1], 'Stock Code'].replace('.NS', '.BO')
+)# Fix for the second stock code
 
     df_quantity = df[[isin_col, 'Quantity']].copy()
     df_quantity.rename(columns={isin_col: 'ISIN'}, inplace=True)
@@ -963,6 +965,7 @@ if uploaded_file is not None:
             else:
 
                 st.error("Failed to generate PDF report. Check logs for details.") 
+
 
 
 
