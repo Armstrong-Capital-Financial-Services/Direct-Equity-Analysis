ebbb5f4db444963339bb259045b5189382e32e35
Armstrong-Capital-Financial-Services <divyanshu@armstrong-cap.com>
2025-11-12 16:35:09 +0530

    Update Direct_Equity_Analysis_2.py

--- Direct_Equity_Analysis_2.py
+++ Direct_Equity_Analysis_2.py
@@ -597,12 +597,13 @@ def create_enhanced_investment_report(equity_df):
         
         # Sector-wise Analysis Table
         elements.append(Paragraph("Detailed Portfolio Analysis", subtitle_style))
-        equity_df_short = equity_df[['Stock Name', '1Y Return (%)', 'Weight_2', 'Score','Revised Score']].copy()
+        equity_df_short = equity_df[['Stock Name', 'Weightage', 'Weight_2', 'Score','Revised Score']].copy()
         equity_df_short['Score'] = equity_df_short['Score'].round(2)
         equity_df_short['Revised Score'] = equity_df_short['Revised Score'].round(2)
+        equity_df_short['Weightage'] = equity_df_short['Weightage'].round(2)
         equity_df_short['Weight_2'] = equity_df_short['Weight_2'].round(2).apply(format_currency)
         equity_df_short = equity_df_short.sort_values(by='Revised Score', ascending=False)
-        header = ['Stock Name', '1Y Return (%)', 'Invested Amt', 'Score', 'Revised Score']
+        header = ['Stock Name', 'Weightage', 'Invested Amt', 'Score', 'Revised Score']
         data_rows = equity_df_short.astype(str).values.tolist()
         table_data = [header] + data_rows
         portfolio_table = Table(table_data, colWidths=[2.8 * inch, 1.2 * inch, 1 * inch, 1 * inch])
@@ -969,6 +970,7 @@ if uploaded_file is not None:
             else:
 
                 st.error("Failed to generate PDF report. Check logs for details.") 
+
 
 
 
